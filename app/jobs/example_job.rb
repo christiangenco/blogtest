c3e2faec7a1a8f7a1078b453f6aa3ffad731a4b5
Christian Genco <christian.genco@gmail.com>
2016-01-05 20:13:55 -0600

    add activejob

---
+++ app/jobs/example_job.rb
@@ -0,0 +1,20 @@
+# invoke with:
+# ExampleJob.perform_later(args)
+# ExampleJob.set(wait_until: Date.tomorrow.noon).perform_later(args)
+
+class ExampleJob < ActiveJob::Base
+  queue_as :default
+
+  def perform(*args)
+    # Do something later
+    "performing "
+    puts "sleeping 5 seconds"
+    sleep 5
+    puts "slept 5 seconds"
+  end
+end
+
+
+class Rofl
+
+end
